f3601acaaae122d64265d508aed0a668209de99e
khasan <xozzslip@gmail.com>
2021-12-07 21:38:24 +0300

    update merge upstream script

--- .circleci/upstream-merge.bash
+++ .circleci/upstream-merge.bash
@@ -3,6 +3,7 @@ UPSTREAM_REPO="Fantom-foundation/go-opera"
 UPSTREAM_URL="https://github.com/$UPSTREAM_REPO"
 git remote get-url upstream &> /dev/null && git remote remove upstream
 git remote add upstream $UPSTREAM_URL
-git fetch --tags upstream
-LATEST_RELEASE_TAG=`curl --silent https://api.github.com/repos/$UPSTREAM_REPO/releases/latest | jq --raw-output .target_commitish`
-git merge upstream/$LATEST_RELEASE_TAG
+git fetch upstream
+LATEST_RELEASE_TAG=`curl --silent https://api.github.com/repos/$UPSTREAM_REPO/releases/latest | jq --raw-output .tag_name`
+git merge $LATEST_RELEASE_TAG -m "release $LATEST_RELEASE_TAG"
+git push origin feature/full-block-api
